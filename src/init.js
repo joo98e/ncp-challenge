52d306ce2fa31152d9ff9350b03bb08c9542ed36
joo98e <jtbeok@gmail.com>
2023-04-02 01:44:40 +0900

    feature : ncp initialize

---
+++ src/init.js
@@ -0,0 +1,11 @@
+import "regenerator-runtime";
+import "dotenv/config";
+import "./db";
+import app from "./server";
+
+const PORT = process.env.PORT || 3000;
+
+const handleListening = () =>
+    console.log(`✅ Server listening on http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
