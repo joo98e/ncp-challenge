f7d236ee52bed2e22142f0c77269728b59f3cb3a
joo98e <jtbeok@gmail.com>
2023-04-12 02:38:05 +0900

    feature : player #3 feature/ui

--- src/client/js/common/common.js
+++ src/client/js/common/common.js
@@ -1,5 +1,43 @@
 /* global $ */
 
+/**
+ *
+ * @type {Song}
+ */
+let currentSong = {
+  title: null,
+  artist: null,
+  src: null,
+  thumbnailSrc: null,
+  tags: [],
+  totalTime: null,
+  likeCount: null,
+  viewCount: null,
+  createdAt: null,
+  updatedAt: null,
+};
+
 // global UI Selector
-window.addEventListener("load", function () {
-});
+window.addEventListener("load", function () {});
+
+/**
+ *
+ * @param song {Song}
+ */
+function setCurrentSong(song) {
+  return (currentSong = {
+    ...song,
+  });
+}
+
+/**
+ *
+ * @param song {Song}
+ */
+function updateCurrentSongUI(song) {
+  $("#song-container .song-thumbnail").attr("src", song.thumbnailSrc);
+  $("#song-container .song-title").text(song.title);
+  $("#song-container .song-artist").text(song.artist);
+  $("#song-container .song-viewCount .count").text(song.viewCount);
+  $("#song-container .song-likeCount .count").text(song.likeCount);
+}
--- src/client/js/views/player.js
+++ src/client/js/views/player.js
@@ -1,5 +1,19 @@
 /* global $ */
 
+/**
+ * @typedef Song
+ * @property {?string} title
+ * @property {?string} artist
+ * @property {?string} src
+ * @property {?string} [thumbnailSrc]
+ * @property {string[]} [tags]
+ * @property {?number} [totalTime]
+ * @property {?number} [likeCount]
+ * @property {?number} [viewCount]
+ * @property {?Date} createdAt
+ * @property {?Date} updatedAt
+ */
+
 $(document).ready(function () {
   $(".main-tab-2").show();
 });
